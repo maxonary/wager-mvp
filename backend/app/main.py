c7a84f7ae7b75d1f7e158695433c40a38f962c47
Maximilian Arnold <62939182+maxonary@users.noreply.github.com>
2024-07-07 19:10:47 -0700

    Add getData endpoint to retrieve battle log for a player

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,6 @@
 from fastapi import FastAPI
-from app.api.endpoints import bets, leaderboard
-from app.database import engine
-import app.models as models
-
-models.Base.metadata.create_all(bind=engine)
+from app.api.endpoints import getData
 
 app = FastAPI()
 
-app.include_router(bets.router, prefix="/api/v1")
-app.include_router(leaderboard.router, prefix="/api/v1")
+app.include_router(getData.router, prefix="/api/v1")
