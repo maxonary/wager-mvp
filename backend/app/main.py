3a047f2db1eb0367b11de798c1f67084f2fcac84
edwin <nuoedwin@gmail.com>
2024-08-03 21:26:57 -0700

    Updated createMatch

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 from app.api.endpoints import getData
 from app.api.endpoints import createUser
+from app.api.endpoints import createMatch
 
 app = FastAPI()
 
 app.include_router(getData.router, prefix="/api/v1")
 app.include_router(createUser.router, prefix="/api/v1")
+app.include_router(createMatch.router, prefix="/api/v1")
