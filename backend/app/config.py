108ed37faf9f8f5a6a40ae54ecc1d1ece35aac11
Maximilian Arnold <62939182+maxonary@users.noreply.github.com>
2024-08-04 16:19:48 -0700

    hotfix: Update API key in config file

--- backend/app/config.py
+++ backend/app/config.py
@@ -4,6 +4,6 @@ from dotenv import load_dotenv
 def load_env():
     load_dotenv()
     return {
-        'API_KEY': os.getenv('API_KEY_Campus'),
+        'API_KEY': os.getenv('API_KEY'),
         "MONGO_URI": os.getenv("MONGO_URI")
     }
