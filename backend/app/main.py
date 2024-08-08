789143892be22ec34abedbd791d40860c243df68
Maximilian Arnold <62939182+maxonary@users.noreply.github.com>
2024-08-08 14:31:05 -0700

    feat: Update API endpoint import in main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from app.api.endpoints import getData
+from app.api.endpoints import getIP
 from app.api.endpoints import createUser
 from app.api.endpoints import createMatch
 from app.api.endpoints import getMatchResult
@@ -8,6 +8,8 @@ from app.api.endpoints import getLeaderBoard
 app = FastAPI()
 
 # app.include_router(getData.router, prefix="/api/v1")
+
+app.include_router(getIP.router, prefix="/api/v1")
 app.include_router(createUser.router, prefix="/api/v1")
 app.include_router(createMatch.router, prefix="/api/v1")
 app.include_router(getMatchResult.router, prefix="/api/v1")
