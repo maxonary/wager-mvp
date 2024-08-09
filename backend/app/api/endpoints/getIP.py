48569aff56318e37bbb6e71c9bafda5fc777612c
Maximilian Arnold <62939182+maxonary@users.noreply.github.com>
2024-08-09 09:32:31 -0700

    hotfix: Update getIP endpoint to use curlmyip.org

--- backend/app/api/endpoints/getIP.py
+++ backend/app/api/endpoints/getIP.py
@@ -5,7 +5,7 @@ router = APIRouter()
 
 @router.get('/ip')
 async def get_ip():
-    ip = requests.get('https://ipv4.ifconfig.me').text
+    ip = requests.get('https://curlmyip.org').text
     return {
         'ip': ip,
     } 
