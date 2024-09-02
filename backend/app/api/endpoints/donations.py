de8c5fbce87a57c9bbd5779617996174ddc7f575
Maximilian Arnold <62939182+maxonary@users.noreply.github.com>
2024-09-02 19:27:48 +0200

    chore: Add API endpoint to fetch all transactions

--- backend/app/api/endpoints/donations.py
+++ backend/app/api/endpoints/donations.py
@@ -1,7 +1,8 @@
-# File: api/endpoints/donations.py
-
 from fastapi import APIRouter, HTTPException
-from app.services.donation_service import fetch_and_process_donations
+from typing import List
+from app.models.transactionModel import Transaction
+from app.services.donation_service import fetch_and_process_donations 
+from app.db import transaction_collection
 
 router = APIRouter()
 
@@ -12,3 +13,16 @@ async def update_donations():
         return {"status": "success", "new_transactions": len(new_transactions)}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+    
+
+@router.get("/transactions", response_model=List[Transaction])
+async def fetch_all_transactions():
+    try:
+        # Fetch all transactions from the transaction collection
+        transactions_cursor = transaction_collection.find({}, {"_id": 0})  # Exclude MongoDB's internal _id field
+        transactions = list(transactions_cursor)
+
+        # Return the transactions in the response
+        return transactions
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"An error occurred: {str(e)}")
